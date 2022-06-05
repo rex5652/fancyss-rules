9aa16e1da2617fa9289b74583c5c62b3c7da00b9
Rex Chen <rex5652@gmail.com>
2022-06-05 09:50:46 +0800

    Update update_rules.sh

--- rules/auto_update/update_rules.sh
+++ rules/auto_update/update_rules.sh
@@ -5,13 +5,6 @@ RULE_PATH=${CURR_PATH%\/*}
 RULE_FILE=${RULE_PATH}/rules.json.js
 OBJECT_1='{}'
 
-prepare(){
-	if ! type -p sponge &>/dev/null; then
-	    printf '%s\n' "error: sponge is not installed, exiting..."
-	    exit 1
-	fi
-}
-
 get_gfwlist(){
 	# gfwlist.conf
 
@@ -244,7 +237,6 @@ finish(){
 }
 
 get_rules(){
-	prepare
 	get_gfwlist
 	get_chnroute
 	get_cdn
